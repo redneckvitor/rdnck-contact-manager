8af9b06c344b566dce6dd3e14740c135447f0201
steingames <joaovitorpessoal@protonmail.com>
2023-10-05 16:04:08 -0300

    Código melhorado

--- source/main.py
+++ source/main.py
@@ -1,18 +1,26 @@
 from tkinter import *
 import customtkinter
 
-class AgendaApp:
+class ContactManager:
     def __init__(self, master):
-        self.master = master
+        self.master = master    # É usado para modificar o root da janela
         self.master.geometry('1280x720')
         self.configure_gui()
+        self.master.title("Agenda de Contatos")
+        self.set_window_icon()
+
 
     def configure_gui(self):
-        # Aqui você pode adicionar os widgets e configurações adicionais da GUI
         pass
+
+    def set_window_icon(self):
+        try:
+            self.master.iconbitmap('assets/window_icon.ico')
+        except:
+            pass
 
 
 if __name__ == "__main__":
     root = customtkinter.CTk()
-    app = AgendaApp(root)
+    app = ContactManager(root)
     root.mainloop()
