a6644669c0bbcd28f3dd36d994b952bab7a30f06
steingames <joaovitorpessoal@protonmail.com>
2023-10-05 19:17:51 -0300

    Código melhorado

--- source/main.py
+++ source/main.py
@@ -1,14 +1,15 @@
+import csv
 from tkinter import *
 import customtkinter
 
 class ContactManager:
     def __init__(self, master):
-        self.master = master    # É usado para modificar o root da janela
+        self.master = master  # É usado para modificar o root da janela
         self.master.geometry('1280x720')
         self.configure_gui()
         self.master.title("Agenda de Contatos")
         self.set_window_icon()
-
+        self.initialize_csv()
 
     def configure_gui(self):
         pass
@@ -19,6 +20,15 @@ class ContactManager:
         except:
             pass
 
+    def initialize_csv(self):
+        filepath = 'contacts.csv'
+        try:
+            with open(filepath, 'r', encoding='utf-8-sig') as file:
+                pass
+        except FileNotFoundError:
+            with open(filepath, 'w', newline='', encoding='utf-8-sig') as file:
+                writer = csv.writer(file, delimiter=',')
+                writer.writerow(['Nome', 'Telefone'])
 
 if __name__ == "__main__":
     root = customtkinter.CTk()
