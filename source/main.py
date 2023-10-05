aeeffd5ee8cd8d1b347db0e3200f110ca8475f1f
steingames <joaovitorpessoal@protonmail.com>
2023-10-05 15:26:41 -0300

    estrutura básica criada

--- source/main.py
+++ source/main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
+from tkinter import *
+import customtkinter
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class AgendaApp:
+    def __init__(self, master):
+        self.master = master
+        self.master.geometry('1280x720')
+        self.configure_gui()
+
+    def configure_gui(self):
+        # Aqui você pode adicionar os widgets e configurações adicionais da GUI
+        pass
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    root = customtkinter.CTk()
+    app = AgendaApp(root)
+    root.mainloop()
