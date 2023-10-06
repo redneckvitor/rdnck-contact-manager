c343422b398176a18747c012e3a01fcc8596dd96
steingames <joaovitorpessoal@protonmail.com>
2023-10-06 16:52:45 -0300

    Novas funções adicionadas

--- source/main.py
+++ source/main.py
@@ -4,15 +4,19 @@ import customtkinter
 
 
 class ContactManager:
+    """ Classe main do programa, responsável por gerenciar a janela
+    e processos"""
+
     def __init__(self, master):
-        self.master = master  # É usado para modificar o root da janela
+        self.master = master  # Se refere a janela 'root'
         self.master.geometry('1280x720')
-        self.configure_gui()
+        self.manage_components()
         self.master.title("Agenda de Contatos")
         self.set_window_icon()
         self.initialize_csv()
 
-    def configure_gui(self):
+    def manage_components(self):
+        """ Gerencia os componentes da interface e seus estados"""
         self.add_contact_component = AddContactComponent(self.master)
 
     def set_window_icon(self):
@@ -22,6 +26,8 @@ class ContactManager:
             pass
 
     def initialize_csv(self):
+        """ Inicializa a tabela de contatos, verificando se a tabela existe,
+        caso não exista, cria uma nova tabela"""
         filepath = 'contacts.csv'
         try:
             with open(filepath, 'r', encoding='utf-8-sig') as file:
@@ -33,21 +39,25 @@ class ContactManager:
 
 
 class Component(customtkinter.CTkFrame):
+    """ Classe pai dos componentes da interface gráfica"""
+
     def __init__(self, parent):
-        """ Abaixo estão largura e altura máximas do componente, mas o Tkinter
-                renderiza os componentes ocm o tamanho mínimo necessário de acordo com
-                os tamanhos fixos dos compoentes, então caso queria mudar o tamanho do componente,
-                é necessário mudar os tamanhos fixos de seus componentes internos"""
+        """ O tamanho dos componentes é definido pelo tamanho mínimo
+        necessário para renderizar seus elementos que tem tamanhos fixos
+        a largura e altura não são definidos diretamente no componente"""
         super().__init__(parent, width=1280, height=720)
 
         self.place(relx=0.5, rely=0.5, anchor="center")  # Centraliza o componente
 
 
 class AddContactComponent(Component):
+    """ Componente de adicionar novos contatos, contém alguns campos e um botão para
+    adicionar novos contatos na tabela de contatos"""
+
     def __init__(self, parent):
         super().__init__(parent)
 
-        # Título H1 "Adicionar novos contatos"
+        # Título "Adicionar novos contatos"
         self.title_label = customtkinter.CTkLabel(self, text="Adicionar novo contato", font=("Arial", 40))
         self.title_label.pack(pady=40, padx=40)
 
@@ -57,6 +67,7 @@ class AddContactComponent(Component):
         # Campo para escrever o nome
         self.name_entry = customtkinter.CTkEntry(self, width=400, height=32)
         self.name_entry.pack(pady=10)
+        self.name_entry.bind("<Return>", lambda event=None: self.save_contact())
 
         # Label do número de telefone
         self.phone_label = customtkinter.CTkLabel(self, text="Telefone:", font=("Arial", 24))
@@ -65,6 +76,7 @@ class AddContactComponent(Component):
         self.phone_entry = customtkinter.CTkEntry(self, width=400, height=32, validate="key",
                                                   validatecommand=(self.register(self._validate_phone), "%P"))
         self.phone_entry.pack(pady=0)
+        self.phone_entry.bind("<Return>", lambda event=None: self.save_contact())
 
         # Label para mostrar mensagens de erro
         self.error_message = customtkinter.CTkLabel(self, text="", font=("Arial", 16))
@@ -78,9 +90,11 @@ class AddContactComponent(Component):
         self.save_button.pack(pady=60)
 
     def _validate_phone(self, value):
+        """ Verifica se o número de telefone contém letras"""
         return value.isdigit() or value == ""
 
     def _phone_exists(self, phone):
+        """ Verifica se o número de telefone digitado já existe na tabela"""
         with open('contacts.csv', 'r', encoding='utf-8-sig') as file:
             reader = csv.reader(file)
             for row in reader:
@@ -89,6 +103,11 @@ class AddContactComponent(Component):
         return False
 
     def save_contact(self):
+        """ Adiciona um novo contato na tabela de contatos com os
+        dados fornecidos pelo usuário, o único campo obrigatório é
+        o campo de telefone, caso o usuário não forneça um nome o 
+        programa adiciona o número como 'desconhecido' na tabela"""
+
         name = self.name_entry.get()
         phone = self.phone_entry.get()
 
@@ -104,7 +123,6 @@ class AddContactComponent(Component):
                 self.phone_entry.delete(0, END)
                 self.error_message.configure(text="")
             else:
-                # Se o telefone já existir, mostra uma mensagem de erro
                 self.error_message.configure(text="Erro: O número de telefone já existe!")
         else:
             self.error_message.configure(text="Erro: O campo de telefone é obrigatório!")
